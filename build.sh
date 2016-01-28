55d7e9d4fc685169221617055258ef870c53ac6e
Fumio KANEHIRO <fkanehiro@gmail.com>
2016-01-28 18:34:13 +0900

    put generated files on the top directory

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-rm -r _build
+rm -r _build _sources _static
 make gettext
 sphinx-intl update -p _build/locale
 sphinx-intl build
 make -e SPHINXOPTS="-D language='ja'" html
-mv _build/html _html-ja
-make html
-mv _html-ja _build/html-ja
+mv _build/html/* .
+rm -rf _build
+
 
